6d3551f33772c9ae5a26921668a2a868572f9a76
Edouard <edouardclisson@gmail.com>
2022-01-18 11:22:10 +0100

    Setup project structure + babel / eslint alias
    _

--- APP/GamificationApp/.eslintrc.js
+++ APP/GamificationApp/.eslintrc.js
@@ -1,16 +1,22 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          _assets: "./src/assets",
+          _components: "./src/components",
+          _constants: "./src/constants",
+          _navigations: "./src/navigation",
+          _screens: "./src/screens",
+          _services: "./src/services",
+          _styles: "./src/styles",
+          _utils: "./src/utils"
+        },
       },
     },
-  ],
+  },
 };
